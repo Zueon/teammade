4358f38b6dbf711842b230e54011092adc6d0bf8
Zueon <maineon0701@gmail.com>
2022-12-12 16:57:19 +0900

    first commit

---
+++ src/main/java/com/mztm/teammade/TeamMadeApplication.java
@@ -0,0 +1,13 @@
+package com.mztm.teammade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mztm/teammade/TeamMadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mztm.teammade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
